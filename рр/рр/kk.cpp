911e176cc18c6c0e5f45b71f198e55fbc9d4add4
efdsf <gfhdfsghgdhhffgh>
2016-12-09 18:17:49 +0400

    стр

---
+++ рр/рр/kk.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int f(int a)
+{
+	if (a==1)
+		return 1;
+	else
+		return f(a-1)*a;
+	
+}
+void main()
+{
+	int c=f(3);
+	cout<<c;
+	system("pause");
+}
